01b529a4f0df1eb01f5750c1d5cf10f0b1eaa263
MadBowJam <madbowjam@gmail.com>
2024-05-01 13:21:10 +0300

    some fixes in code

--- src/components/functions/RenewData.js
+++ src/components/functions/RenewData.js
@@ -12,10 +12,7 @@ const RenewData = () => {
       // for (let i = 0; i < itemsList.length; i++) {
       for (let i = 0; i < 5; i++) {
         const term = itemsList[i].nameForFetch;
-        
         const encodedTerm = encodeURIComponent(term);
-        
-        console.log(encodedTerm)
         
         const response = await axios.get(`https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=${encodedTerm}`);
         // const response = await axios.get(`https://steamcommunity.com/market/listings/730/${encodedTerm}`);
@@ -23,11 +20,9 @@ const RenewData = () => {
 
         
         console.log(response)
-        // if (matchingItem) {
-          // Оновлюємо ціну у списку предметів, видаляючи перший символ
-          itemsList[i].price = parseFloat(response.data.lowest_price.slice(1));
-          console.log(itemsList[i].price)
-        toast.success(`Price for ${term}: ${itemsList[i].price}`, { autoClose: 3000 }); // autoClose: 3000 означає, що сповіщення буде закрите через 3 секунди
+        itemsList[i].price = parseFloat(response.data.lowest_price.slice(1));
+        console.log(itemsList[i].price)
+        toast.success(`Price for ${term}: ${itemsList[i].price}`, { autoClose: 5000 }); // autoClose: 3000 означає, що сповіщення буде закрите через 3 секунди
         // }
         
         // Затримка перед наступним запитом
@@ -39,6 +34,16 @@ const RenewData = () => {
       console.log('Updated item list:', itemsList);
     } catch (error) {
       console.error('Error renewing data:', error);
+      // Додайте логіку обробки помилок тут
+    }
+    
+    try {
+      const response = await axios.post('/update-items-list', itemsList); // Виконання POST-запиту на сервер
+      
+      // Отримання повідомлення про успішну операцію від сервера
+      toast.success(response.data.message, { autoClose: 3000 });
+    } catch (error) {
+      console.error('Error updating item list:', error);
       // Додайте логіку обробки помилок тут
     }
   };
